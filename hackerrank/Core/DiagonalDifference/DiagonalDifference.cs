a9d060cf791403ddadea5edce1f9b432425ee81a
Sadiel <sadiel.jarvis@shrss.com>
2019-09-14 10:44:40 -0400

    https://www.hackerrank.com/challenges/diagonal-difference/problem

---
+++ hackerrank/Core/DiagonalDifference/DiagonalDifference.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.DiagonalDifference
+{
+    public class DiagonalDifference
+    {
+        public int diagonalDifference(List<List<int>> matrix)
+        {
+            var firstDiagonal = 0;
+            var secondDiagonal = 0;
+
+            for (var i = 0; i < matrix.Count; i++)
+            {
+                firstDiagonal += matrix[i][i];
+                secondDiagonal += matrix[i][matrix.Count - 1 - i];
+            }
+
+            return Math.Abs(firstDiagonal - secondDiagonal);
+        }
+    }
+}
---
+++ hackerrank/Tests/DiagonalDifferenceTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DiagonalDifference;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace Tests
+{
+    [TestFixture]
+    public class DiagonalDifferenceTests
+    {
+        [Test]
+        public void Test()
+        {
+            //Arrange
+            var matrixOrder = 3;
+            var matrix = new List<List<int>>
+            {
+                new List<int> {11, 2, 4},
+                new List<int> {4, 5, 6},
+                new List<int> {10, 8, -12}
+            };
+
+            //Act
+            var sut = new DiagonalDifference();
+            var result = sut.diagonalDifference(matrix);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(15));
+
+        }
+
+        [Test]
+        public void Test2()
+        {
+            //Arrange
+            var matrixOrder = 4;
+
+            var matrix = new List<List<int>>
+            {
+                new List<int> {-1, 1, -7, -8},
+                new List<int> {-10, -8, -5, -2},
+                new List<int> {0, 9, 7, -1},
+                new List<int> { 4, 4, -2, 1}
+            };
+
+            //Act
+            var sut = new DiagonalDifference();
+            var result = sut.diagonalDifference(matrix);
+
+
+            //Assert
+            Assert.That(result, Is.EqualTo(1));
+
+        }
+    }
+}
