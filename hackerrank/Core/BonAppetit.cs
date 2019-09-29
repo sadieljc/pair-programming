24f977857829271780d949985f2eae50da729344
Sadiel <sadiel.jarvis@shrss.com>
2019-09-29 11:24:17 -0400

    https://www.hackerrank.com/challenges/bon-appetit/problem

---
+++ hackerrank/Core/BonAppetit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Core
+{
+    public class BonAppetit
+    {
+        public string Evaluate(List<int> bill, int anaDidntLike, int amountCharged)
+        {
+            var amountForSharedItems = 0;
+
+            for (var i = 0; i < bill.Count; i++)
+            {
+                if (i != anaDidntLike)
+                    amountForSharedItems += bill[i];
+            }
+
+            var evenSplittedBillForSharedItems = amountForSharedItems / 2;
+
+            return evenSplittedBillForSharedItems == amountCharged 
+                ? "Bon Appetit" 
+                : (amountCharged - evenSplittedBillForSharedItems).ToString();
+        }
+    }
+}
---
+++ hackerrank/Tests/BonAppetitTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Core;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class BonAppetitTests
+    {
+        private readonly BonAppetit _bonAppetit = new BonAppetit();
+
+        [Test]
+        public void ReturnAmountToRefund_When_NotEvenlySplitted()
+        {
+            //Arrange
+            var bill = new List<int> {3, 10, 2, 9};
+            var anaDidntLike = 1;
+            var amountCharged = 12;
+
+            //Act
+            var result = _bonAppetit.Evaluate(bill, anaDidntLike, amountCharged);
+
+            //Assert
+            Assert.That(result, Is.EqualTo("5"));
+        }
+
+        [Test]
+        public void ReturnBonAppetit_When_EvenlySplitted()
+        {
+            //Arrange
+            var bill = new List<int> { 3, 10, 2, 9 };
+            var anaDidntLike = 1;
+            var amountCharged = 7;
+
+            //Act
+            var result = _bonAppetit.Evaluate(bill, anaDidntLike, amountCharged);
+
+            //Assert
+            Assert.That(result, Is.EqualTo("Bon Appetit"));
+        }
+    }
+}
