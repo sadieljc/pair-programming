00aab4feaae28dc2fb3942e876a6d2f4ff405bd9
Sadiel Jarvis <sadieljc@hotmail.com>
2019-11-03 11:34:52 -0500

    Refactoring SherlockAndArray console code into its own class.

--- hackerrank/CSharp/Main/Program.cs
+++ hackerrank/CSharp/Main/Program.cs
@@ -1,39 +1,15 @@
-﻿using Core;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Main.SherlockAndArray;
 
 namespace Main
 {
     public class Program
     {
+        private const string Path = "..\\..\\SherlockAndArray\\TestCase3_oneLine.txt";
+
         static void Main(string[] args)
         {
-            var bigList = new List<int>();
-
-            using (var reader = new StreamReader("..\\..\\SherlockAndArray\\TestCase3_oneLine.txt"))
-            {
-                bigList = reader.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
-
-                var sut = new SherlockAndArray();
-
-                var start = DateTime.Now;
-                Console.WriteLine(start);
-
-                string result = sut.BalancedSum(bigList);
-                Console.WriteLine(result);
-
-                var end = DateTime.Now;
-                Console.WriteLine(end);
-
-                var difference = end.Subtract(start);
-                Console.WriteLine(difference.TotalSeconds);                
-            }
-
-            System.Console.ReadLine();
+            var service = new SherlockAndArrayService();
+            service.Execute(Path);
         }
     }
 }
